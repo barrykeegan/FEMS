15744c595fc1bd07c2a82131e56e0ffc606e10a9
beazzlebub <barrykeegan@gmail.com>
2019-05-27 00:06:57 +0100

    Added click listeners to case list items and buttons.

--- app/src/main/java/org/leoapps/fems/CaseListAdapter.java
+++ app/src/main/java/org/leoapps/fems/CaseListAdapter.java
@@ -5,31 +5,71 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
 public class CaseListAdapter extends RecyclerView.Adapter<CaseListAdapter.ViewHolder> {
     private List<Case> caseList;
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public TextView tvCaseListID;
         public TextView caseListReference;
         public TextView caseListOperation;
         public TextView caseListType;
         public TextView caseListDate;
         public TextView caseListLocation;
         public View layout;
+        public ImageView imgShareCase;
+        public ImageView imgEditCase;
+        public ImageView imgDeleteCase;
+        public LinearLayout llCaseDetails;
 
         public ViewHolder(View v)
         {
             super(v);
             layout = v;
 
+            tvCaseListID = v.findViewById(R.id.tv_case_list_case_id);
             caseListReference = v.findViewById(R.id.tv_case_list_reference);
             caseListOperation = v.findViewById(R.id.tv_case_list_operation);
             caseListType = v.findViewById(R.id.tv_case_list_type);
             caseListDate = v.findViewById(R.id.tv_case_list_date);
             caseListLocation = v.findViewById(R.id.tv_case_list_location);
+
+            llCaseDetails = v.findViewById(R.id.ll_case_list_detail);
+            imgShareCase = v.findViewById(R.id.img_case_list_share_case);
+            imgEditCase = v.findViewById(R.id.img_case_list_edit_case);
+            imgDeleteCase = v.findViewById(R.id.img_case_list_delete_case);
+
+            llCaseDetails.setOnClickListener(this);
+            imgShareCase.setOnClickListener(this);
+            imgEditCase.setOnClickListener(this);
+            imgDeleteCase.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            String whichCase = "Case " + tvCaseListID.getText().toString();
+            if( v == llCaseDetails)
+            {
+                Toast.makeText(v.getContext(), "Details for... " + whichCase, Toast.LENGTH_LONG).show();
+            }
+            if (v == imgDeleteCase)
+            {
+                Toast.makeText(v.getContext(), "Deleting... " + whichCase, Toast.LENGTH_LONG).show();
+            }
+            if(v == imgEditCase)
+            {
+                Toast.makeText(v.getContext(), "Editing... " + whichCase, Toast.LENGTH_LONG).show();
+            }
+            if(v == imgShareCase)
+            {
+                Toast.makeText(v.getContext(), "Sharing... " + whichCase, Toast.LENGTH_LONG).show();
+            }
         }
     }
 
@@ -47,6 +87,7 @@ public class CaseListAdapter extends RecyclerView.Adapter<CaseListAdapter.ViewHo
     @Override
     public void onBindViewHolder(@NonNull CaseListAdapter.ViewHolder holder, int position) {
         final Case aCase = caseList.get(position);
+        holder.tvCaseListID.setText(Integer.toString(aCase.ID));
         if(aCase.ReferenceID == null || aCase.ReferenceID.isEmpty())
         {
             holder.caseListReference.setText(R.string.value_not_set);
